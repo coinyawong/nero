2e5db4e572f2466dc3bfb3c3b1021874a323288a
coinyawong <nero2018@naver.com>
2018-08-18 23:28:18 +0900

    modify

--- src/main.py
+++ src/main.py
@@ -8,14 +8,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "야웅 베이킹에 오신 것을 환영합니다."
+    return render_template("index.html")
 
-@app.route('/list/<cycle>')
+@app.route('/list/<int:cycle>')
 def list(cycle):
+
 	conn = pymysql.connect(host='127.0.0.1', user='coinyawong2', password='1010893!', db='yawong', charset='utf8mb4')
 	cur = conn.cursor()
-	sql= 'select a.id, a.name, a.i_address, a.cycle, a.balance, truncate(a.balance/(b.roll*10000)*100,3) as percent, truncate(a.balance/(b.roll*10000)*b.total*0.945,3) reward from user a, cycle b where a.cycle+7 <= %s and b.cycle = %s'
-	cur.execute(sql, (cycle, cycle))
+	sql= 'select a.id, a.name, a.i_address, a.cycle, a.balance, truncate(a.balance/(b.roll*10000)*100,3) as percent, truncate(a.balance/(b.roll*10000)*b.total*0.945,3) as reward from user a, cycle b where a.cycle+7 <= %s and b.cycle = %s'
+
+	if(cycle<10):
+	  cur.execute(sql, (10, 10))
+	else:
+	  cur.execute(sql, (cycle, cycle))
 	result = cur.fetchall()
 	templateData = {'data' : result}
 	return render_template('user.html',**templateData)
