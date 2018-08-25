2f268fbf82079382f6dd417a6b035955dda55bf6
coinyawong <nero2018@naver.com>
2018-08-25 18:10:50 +0000

    update

--- con.sh
+++ con.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 echo "####################################"
-echo "1:환경설정, 2: push, 3:pull, 4: DB연결, 5: DB백업"
+echo "1:서버시작, 2: 서버 off,  3: push, 4:pull, 5: DB연결, 6: DB백업"
 
 read -p "명령을 선택 : " a
 
 case $a in
-1) ##activate
-source yawong_baking/bin/activate;;
-2) ## push
+1) ##서버시작
+nohup ./yawong_baking/bin/python2.7 ./src/main.py > server.log;;
+2) ##서버오프
+pkill -9 -ef main.py;;
+3) ## push
 git push origin master;;
-3) ## pull
+4) ## pull
 git pull origin master;;
-4) ## db연결
+5) ## db연결
 mysql -u coinyawong2 -p;;
-5) ## db백업
+6) ## db백업
 mysqldump -u coinyawong2 -p --all-databases > $(date +%Y%m%d).dump;;
 *)
 echo "잘못 입력하셨습니다."
--- src/main.py
+++ src/main.py
@@ -18,6 +18,12 @@ def index():
      cur.close()
      conn.close()
 
+@app.route('/admin')
+def admin():
+        conn = pymysql.connect(host='localhost', user='coinyawong2', password='10-10893', db='yawong', charset='utf8mb4')
+        return render_template('admin.html')
+        conn.close()
+
 @app.route('/list', methods=['GET'])
 def list():
 	cycle = request.args.get('cycle')
@@ -25,13 +31,57 @@ def list():
 	  cycle = 10
 	conn = pymysql.connect(host='localhost', user='coinyawong2', password='10-10893', db='yawong', charset='utf8mb4')
 	cur = conn.cursor()
-	sql= 'select a.name name, a.address address, b.cycle cycle, b.balance balance, truncate(b.balance/(c.roll*10000)*100,3) as percent, truncate(b.balance/(c.roll*10000)*c.total*0.945,3) as reward from user a, user_info b, cycle c where a.address = b.address and b.cycle+7 <= %s and c.cycle = %s and b.balance > 0'
+	sql= 'select a.name name, a.address address, b.cycle cycle, b.balance balance, truncate(b.balance/(c.roll*10000)*100,3) as percent, truncate(b.balance/(c.roll*10000)*c.total*0.945,3) as reward, c.chk from user a, user_info b, cycle c where a.address = b.address and b.cycle+7 <= %s and c.cycle = %s and b.balance > 0 order by b.cycle'
 	cur.execute(sql, (cycle, cycle))
 	result = cur.fetchall()
-	templateData = {'data' : result}
+	templateData = {'data' : result, 'cycle' : cycle}
 	return render_template('user.html',**templateData)
 	cur.close()
 	conn.close()
+
+@app.route('/reward', methods=['GET'])
+def reward():
+	cycle = request.args.get('cycle')
+        conn = pymysql.connect(host='localhost', user='coinyawong2', password='10-10893', db='yawong', charset='utf8mb4')
+        cur = conn.cursor()
+	if len(cycle) ==0:
+	   sql= 'select cycle, roll, total, chk from cycle order by cycle'
+	   cur.execute(sql)
+	else:
+	   sql='select cycle, roll, total, chk from cycle where cycle=%s'
+           cur.execute(sql, cycle)
+        result = cur.fetchall()
+        templateData = {'data' : result, 'cycle' : cycle}
+        return render_template('reward.html',**templateData)
+        cur.close()
+        conn.close()
+
+@app.route('/user_reward', methods=['GET'])
+def user_reward():
+        id = request.args.get('id')
+        conn = pymysql.connect(host='localhost', user='coinyawong2', password='10-10893', db='yawong', charset='utf8mb4')
+        cur = conn.cursor()
+        sql='select a.cycle, a.address, (select sum(balance) from user_info where address=a.address and cycle+7<=a.cycle) balance, a.reward from payout a where a.address=%s'
+        cur.execute(sql, id)
+        result = cur.fetchall()
+	cur.close()
+	cur2 = conn.cursor()
+	sql2='select a.name, truncate(sum(b.reward),3) reward from user a, payout b where a.address = b.address and a.address = %s'
+	cur2.execute(sql2, id)
+	result2 = cur2.fetchall()
+	cur2.close()
+	if id == '-':
+	  cur3 = conn.cursor()
+	  sql3='select truncate(sum(fee),3) fee from fee'
+	  cur3.execute(sql3)
+	  result3 = cur3.fetchall()
+          cur3.close()
+	  templateData = {'data' : result, 'total' : result2, 'fee' : result3}
+	else:
+          templateData = {'data' : result, 'total' : result2}
+        return render_template('user_reward.html',**templateData)
+        conn.close()
+
 
 if __name__ == "__main__":
    app.run(host='192.168.0.136', port=8306, debug=True)
