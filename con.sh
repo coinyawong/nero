76f84827ed1bce220b125c676f26b83c23ecbdbf
coinyawong <nero2018@naver.com>
2018-08-25 09:10:48 +0000

    db백업

--- con.sh
+++ con.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "####################################"
-echo "1:환경설정, 2: push, 3:pull, 4: DB연결"
+echo "1:환경설정, 2: push, 3:pull, 4: DB연결, 5: DB백업"
 
 read -p "명령을 선택 : " a
 
@@ -13,6 +13,8 @@ git push origin master;;
 git pull origin master;;
 4) ## db연결
 mysql -u coinyawong2 -p;;
+5) ## db백업
+mysqldump -u coinyawong2 -p --all-databases > $(date).dump;;
 *)
 echo "잘못 입력하셨습니다."
 esac
