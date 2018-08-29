3a56ac1edd4aa816d10c22d552914ac76c9f7481
coinyawong <30748399+coinyawong@users.noreply.github.com>
2018-08-29 16:24:38 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -163,10 +163,10 @@ def reward():
         conn = pymysql.connect(host='localhost', user='coinyawong2', password='10-10893', db='yawong', charset='utf8mb4')
         cur = conn.cursor()
 	if len(cycle) ==0:
-	   sql= 'select cycle, roll, concat(truncate(total/roll, 3), "ꜩ") r_reward, total, chk from cycle order by cycle'
+	   sql= 'select cycle, roll, concat(truncate(total/roll, 3), "ꜩ") r_reward, concat(total, "ꜩ") total, chk from cycle order by cycle'
 	   cur.execute(sql)
 	else:
-	   sql='select cycle, roll, concat(truncate(total/roll, 3), "ꜩ") r_reward, total, chk from cycle where cycle=%s'
+	   sql='select cycle, roll, concat(truncate(total/roll, 3), "ꜩ") r_reward, concat(total, "ꜩ") total, chk from cycle where cycle=%s'
            cur.execute(sql, cycle)
         result = cur.fetchall()
 	cur2 = conn.cursor()
